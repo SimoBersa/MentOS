f32ade5cb95bc6119c258575483caa329c7b8da6
francesco <francymas23@gmail.com>
2020-06-11 23:57:20 +0200

    [add] scheduling algorithm
    Co-authored-by: SimoBersa <sbersani.info@gmail.com>

--- mentos/src/process/scheduler_algorithm.c
+++ mentos/src/process/scheduler_algorithm.c
@@ -19,55 +19,81 @@ task_struct *pick_next_task(runqueue_t *runqueue, time_t delta_exec)
 #if defined(SCHEDULER_RR)
 	//==== Implementatin of the Round-Robin Scheduling algorithm ============
 
+	// get the next process after the current one
+	list_head *nNode = runqueue->curr->run_list.next;
 
+	// check if we reached the head of list_head
+	if (nNode == &runqueue->queue)
+		nNode = nNode->next;
+
+	// get the task_struct
+	next = list_entry(nNode, task_struct, run_list);
 
 	//=======================================================================
 #elif defined(SCHEDULER_PRIORITY)
 	//==== Implementatin of the Priority Scheduling algorithm ===============
-
+	
 	// get the first element of the list
-	next = list_entry(/*...*/);
+	next = list_entry(&runqueue->queue, task_struct, run_list);
 
 	// Get its static priority.
-	time_t min = /*...*/
+	unsigned int min = next->se.prio;
 
-	list_head *it;
-	// Inter over the runqueue to find the task with the smallest priority value
+	list_head * it;
+	// Iter over the runqueue to find the task with the smallest priority value
 	list_for_each (it, &runqueue->queue) {
-		task_struct *entry = list_entry(/*...*/);
-		// Check entry has a lower priority
-		if (/*...*/) {
-			/*...*/
+		task_struct *entry = list_entry(it, task_struct, run_list);
+		// Check if the entry has a lower priority
+		if (entry->se.prio <= min) {
+			min = entry->se.prio;
+			next = entry;
 		}
 	}
 
 	//=======================================================================
 #elif defined(SCHEDULER_CFS)
-	//==== Implementatin of the Completely Fair Scheduling ==================
+	//==== Implementation of the Completely Fair Scheduling ==================
 
 	// Get the weight of the current process.
 	// (use GET_WEIGHT macro!)
-	int weight = /*...*/
+	int weight = GET_WEIGHT(runqueue->curr->se.prio);
 
-	if (weight != NICE_0_LOAD) {
+	if (weight != NICE_0_LOAD){
 		// get the multiplicative factor for its delta_exec.
-		double factor = /*...*/
+		double factor = NICE_0_LOAD/weight;
 
 		// weight the delta_exec with the multiplicative factor.
-		delta_exec = // ...
+		delta_exec = delta_exec*factor;
 	}
 
 	// Update vruntime of the current process.
-	// ...
+	runqueue->curr->se.vruntime += delta_exec;
 
-	// Inter over the runqueue to find the task with the smallest vruntime value
-	// ...
+	// Get the first element of the list
+	next = list_entry(&runqueue->queue, task_struct, run_list);
+
+
+	// Get its virtual runtime.
+	time_t min = next->se.vruntime;
+
+	// Iter over the runqueue to find the task with the smallest vruntime value
+	list_head * it;
+	list_for_each (it, &runqueue->queue) {
+		task_struct *entry = list_entry(it, task_struct, run_list);
+		// Check if the entry has a lower priority
+		if (entry->se.vruntime <= min) {
+			min = entry->se.vruntime;
+			next = entry;
+		}
+	}
 
 	//========================================================================
 #else
 #error "You should enable a scheduling algorithm!"
 #endif
-	assert(next && "No valid task selected. Have you implemented a scheduling algorithm?");
+	assert(next && "No valid task selected.");
+
+	//dbg_print("Selected %d\n", next->pid);
 
 	return next;
 }
